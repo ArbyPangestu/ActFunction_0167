6c8775a5517de8a722c26b1d3464e1a27dcf9959
ArbyPangestu <arbypangestu340@gmail.com>
2026-03-02 21:47:16 +0700

    implementasi fungsi dan prosedur

--- Fungsi.cpp
+++ Fungsi.cpp
@@ -4,3 +4,15 @@ using namespace std;
 
 // deklarasi variabel
 int p, l;
+
+// implementasi fungsi dan prosedur
+void input(){
+    cout << "Masukkan Panjang : ";
+    cin >> p;
+    cout << "Masukkan Lebar : ";
+    cin >> l;
+}
+
+int luasPersegi(){
+    return p * l;
+}
